b254f665b990e7af4979043df8401490fd92e62f
rodrigoDev <rodriguinho2456@gmail.com>
2023-07-13 08:48:59 -0300

    feat - created seed Game

--- src/main/java/com/rodrigodev/dslist/entities/Game.java
+++ src/main/java/com/rodrigodev/dslist/entities/Game.java
@@ -3,6 +3,7 @@ package com.rodrigodev.dslist.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,7 +11,7 @@ import jakarta.persistence.Table;
 @Table(name = "tb_game")
 public class Game {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String title;
 
